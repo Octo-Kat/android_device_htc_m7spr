2554237042493c448b83a41dd47baf26fe2bfe7c
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-30 13:59:55 -0500

    Update and rename cm.mk to oct.mk

--- oct.mk
+++ oct.mk
@@ -6,5 +6,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.3/JSS15J/251863.2:user/release-keys" PRIVATE_BUILD_DESC="3.04.651.2 CL251863 release-keys"
 
-PRODUCT_NAME := cm_m7spr
+PRODUCT_NAME := oct_m7spr
 PRODUCT_DEVICE := m7spr
